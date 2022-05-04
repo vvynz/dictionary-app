3bddc67e1baef259332a2296907f1bd0ecd682c1
Vivian Zhang <vvynz12@gmail.com>
2022-05-04 18:53:20 -0400

    Create a search function that will be triggered on form submission

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Dictionary() {
 
 
+  const search = (e) => {
+    e.preventDefault();
+    alert(`Looking up...`);
+  }
+
   return (
-    <form className="text-center">
+    <form className="text-center" onSubmit={search}>
       <input type="search" autoFocus={true} />
     </form>
   );
