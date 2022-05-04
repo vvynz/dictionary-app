e3993c812ab339082809660520965957e4ffc358
Vivian Zhang <vvynz12@gmail.com>
2022-05-04 18:59:30 -0400

    Create handleKeyword function that will setKeyword state to the input received from user

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 
 export default function Dictionary() {
-
+  const [keyword, setKeyword] = useState("");
 
   const search = (e) => {
     e.preventDefault();
-    alert(`Looking up...`);
+    alert(`Looking up ${keyword}...`);
+  }
+
+  const handleKeyword = (e) => {
+    setKeyword(e.target.value);
   }
 
   return (
     <form className="text-center" onSubmit={search}>
-      <input type="search" autoFocus={true} />
+      <input type="search" autoFocus={true} onChange={handleKeyword} />
     </form>
   );
 }
