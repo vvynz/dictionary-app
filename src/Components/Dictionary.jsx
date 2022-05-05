6fbfba4a6f39d9cb6d44fee1b7f9bda84e8379c4
Vivian Zhang <vvynz12@gmail.com>
2022-05-05 18:41:00 -0400

    Create a Components folder and a Results component. Move the Dictionary component into the Components folder

---
+++ src/Components/Dictionary.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Results from "./Results";
+
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState({});
+
+  const handleResponse = (res) => {
+    console.log("RESPONSE", res.data[0]);
+    setResults(res.data[0]);
+  }
+
+  // on submit event, will make an axios call to the api
+  const search = (e) => {
+    e.preventDefault();
+    // alert(`Looking up ${keyword}...`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log("APIURL", apiUrl);
+
+    axios.get(apiUrl)
+      .then(handleResponse)
+      // if word doesn't exist, displays error message
+      .catch((err) => console.log(err.response.data.message));
+  }
+
+  // updates the setKeyword state with user's input
+  const handleKeyword = (e) => {
+    setKeyword(e.target.value);
+  }
+
+  return (
+    <div>
+      <form className="text-center" onSubmit={search}>
+        <input type="search" autoFocus={true} onChange={handleKeyword} />
+      </form>
+      <Results />
+    </div>
+  );
+}
