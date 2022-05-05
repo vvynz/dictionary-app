0e6cf57b664b0ad9bf78c4795f83b9854e1eb252
Vivian Zhang <vvynz12@gmail.com>
2022-05-05 19:19:24 -0400

    Set props.results as a variable. If props.results exists, it will render the word and phonetic else it will return null

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-export default function Results() {
-  return (
-    <h3>🐳　HELLO RESULTS</h3>
-  );
+export default function Results(props) {
+  console.log("PROPS =", props.results);
+  const data = props.results;
+
+  if (data) {
+    return (
+      <div>
+        <h4>{data.word}</h4>
+        <span>
+          <small>{data.phonetic}</small>
+        </span>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
