8c21418ea720fa61a1e828b0db7c2fdd22c1f850
Vivian Zhang <vvynz12@gmail.com>
2022-05-24 17:42:28 -0400

    Add some styling to the header and footer. Included link to github.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import Dictionary from "./Components/Dictionary";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="text-center">Dictionary App</header>
+        <header className="text-center">
+          <Typography variant="h2" component="div" gutterBottom>Dictionary App</Typography></header>
         <main>
           <Dictionary />
         </main>
         <footer className="text-center">
-          <small>Coded by Vivian Zhang.</small>
+          <Typography variant="caption" display="block" mt={2}>Coded by Vivian Zhang and is open sourced on <a href="https://github.com/vvynz/dictionary-app" target="_blank" rel="noreferrer">Github</a>.</Typography>
         </footer>
       </div>
     </div>
