16bc2d8a2939ce5c14a5967c581003d9d6f0b6b7
Vivian Zhang <vvynz12@gmail.com>
2023-02-09 18:39:21 -0500

    Add tests for the definition header

--- cypress/e2e/mainpage.cy.js
+++ cypress/e2e/mainpage.cy.js
@@ -6,4 +6,14 @@ describe('main page', () => {
   it("has a header with the correct text", () => {
     cy.get(".header").contains("dictionary")
   })
+
+  context("definition header", () => {
+    it("displays the default keyword", () => {
+      cy.get(".definition-header").contains("dusk")
+    })
+    it("has an audio player", () => {
+      // this may not be a good test since I'm aware that the api doesn't have an audio file available for every word...
+      cy.get(".react-audio-player").should("exist")
+    })
+  })
 })
