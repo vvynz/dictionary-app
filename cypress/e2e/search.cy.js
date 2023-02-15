f73c2f96224e11f8279d54f2d4d355c991d85229
Vivian Zhang <vvynz12@gmail.com>
2023-02-15 18:06:09 -0500

    Add test for unhappy path, error message should be displayed when word cannot be found

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -3,9 +3,18 @@ describe('search form', () => {
     cy.visit('http://localhost:3000')
   })
 
+  // happy path
   it("allows users to look up a word", () => {
+    // since I'm using the MUI lib, the data attributes won't work. Cypress cannot find them on the MUI components
     cy.get(".MuiInputBase-input").clear().type("book")
     cy.get(".MuiButtonBase-root").eq(1).click()
     cy.getByData("definition-header").contains("book")
+  })
+
+  //unhappy path
+  it.only("displays an error if a word cannot be found", () => {
+    cy.get(".MuiInputBase-input").clear().type("pari")
+    cy.get(".MuiButtonBase-root").eq(1).click()
+    cy.get(".err-msg").should("exist").contains("Sorry pal, we couldn't find definitions for the word you were looking for.")
   })
 })
